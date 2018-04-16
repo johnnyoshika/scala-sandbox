685dc3804b9791857980413748d3fe513cf2f95a
Johnny Oshika <johnnyoshika@gmail.com>
2018-04-16 07:14:47 -0700

    Update 4.

--- src/main/scala/List.scala
+++ src/main/scala/List.scala
@@ -10,6 +10,11 @@ object ListRunner {
     val b = singleton(1)
     val c = singleton[Boolean](true)
     val d = singleton(true)
+
+    // object List below allows us to call List in this way
+    val e = List
+    val f = List(1)
+    val g = List(1,2)
   }
 }
 
@@ -27,4 +32,10 @@ class Nil[T] extends List[T] {
   def isEmpty = true
   def head = throw new NoSuchElementException("Nil.head")
   def tail = throw new NoSuchElementException("Nil.talk")
+}
+
+object List {
+  def apply[T]() = new Nil
+  def apply[T](x1: T) = new Cons(x1, new Nil)
+  def apply[T](x1: T, x2: T) = new Cons(x1, new Cons(x2, new Nil))
 }
